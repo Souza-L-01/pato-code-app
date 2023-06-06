0bf048a0c8e3d6fd6ba289ae79b7965ac2f80f68
Lucas_Souza <souza.vilela.lucas@gmail.com>
2023-06-06 15:05:17 +0200

    lesson migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_06_122200) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_06_130015) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "lessons", force: :cascade do |t|
+    t.bigint "track_id", null: false
+    t.string "title"
+    t.string "content"
+    t.integer "difficulty"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["track_id"], name: "index_lessons_on_track_id"
+  end
 
   create_table "prompts", force: :cascade do |t|
     t.bigint "user_id", null: false
@@ -40,5 +50,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_06_122200) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "lessons", "tracks"
   add_foreign_key "prompts", "users"
 end
