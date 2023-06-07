f255992bcbc166819e719086d31e355a1301b5f9
Jip Teeuwen <jipteeuwen@hotmail.com>
2023-06-07 17:58:57 +0200

    add cloudinary

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,15 +2,18 @@ puts "Cleaning database..."
 Language.destroy_all
 
 puts "Creating languages..."
-javascript = { name: "JavaScript" }
-ruby =  { name: "Ruby" }
-css = { name: "CSS" }
-html = { name: "HTML" }
+javascript = { name: "JavaScript", filepath: File.join(__dir__, "icons/js-folder.svg") }
+ruby =  { name: "Ruby", filepath: File.join(__dir__, "icons/ruby-folder.svg") }
+css = { name: "CSS", filepath: File.join(__dir__, "icons/CSS-folder.svg") }
+html = { name: "HTML", filepath: File.join(__dir__, "icons/CSS-folder.svg") }
 
 languages = [javascript, ruby, css, html]
 
 languages.each do |attributes|
-  language = Language.create!(attributes)
+  language = Language.create!(name: attributes[:name])
+  file = File.read(attributes[:filepath])
+  language.image.attach(io: file, filename: "#{attributes[:name]}.svg", content_type: "image/svg+xml")
+  language.save
   puts "Created #{language.name}"
 end
 puts "Finished!"
