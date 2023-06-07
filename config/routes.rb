2fb20a8e18ac5ccd332371fa90a1c5627e2bba81
Lucas_Souza <souza.vilela.lucas@gmail.com>
2023-06-07 11:59:19 +0200

    side-bar

--- config/routes.rb
+++ config/routes.rb
@@ -16,7 +16,6 @@ Rails.application.routes.draw do
   resources :prompts, only: [:show, :new, :create]
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
   # Defines the root path route ("/")
   # root "articles#index"
 end
