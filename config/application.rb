2b454c4df566399b7730a2fcd534df79e98a083d
Jip Teeuwen <jipteeuwen@hotmail.com>
2023-06-09 15:41:37 +0200

    Fixed issues with fonts

--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,8 @@ module PatoCodeApp
     end
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
+    
+    config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
 
     # Configuration for the application, engines, and railties goes here.
     #
--- config/routes.rb
+++ config/routes.rb
@@ -8,10 +8,10 @@ Rails.application.routes.draw do
   resources :lessons, only: %i[index show create] do
     collection do
       get 'search'
-    end  
-    resources :posts, only: %i[index show create]
     end
+    resources :posts, only: %i[index show create]
   end
+
   resources :posts, only: %i[destroy] do
     resources :comments, only: %i[create]
   end
@@ -19,9 +19,8 @@ Rails.application.routes.draw do
   resources :comments, only: %i[destroy]
 
   resources :prompts, only: %i[new create show] do
-    collection do 
+    collection do
       post :generate_text
     end
   end
-
 end
