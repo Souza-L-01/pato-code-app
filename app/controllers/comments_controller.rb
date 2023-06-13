1d10bfc9470795d64b1902477ae6a19f1a522847
Jip Teeuwen <jipteeuwen@hotmail.com>
2023-06-13 11:03:43 +0200

    pushing to get synced

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,10 +3,12 @@ class CommentsController < ApplicationController
     @post = Post.find(params[:post_id])
     @comment = Comment.new(comment_params)
     @comment.user = current_user
+    @comment.post = @post
     if @comment.save
       flash[:notice] = "Comment created successfully."
       redirect_to lesson_post_path(@post.lesson.id, @post)
     else
+      raise
       flash[:alert] = "Comments can't be empty."
     end
   end
