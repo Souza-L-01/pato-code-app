b77477620e3b314781b17201521b4b7af9190e3b
Lucas_Souza <souza.vilela.lucas@gmail.com>
2023-06-21 18:07:26 +0200

    fix sidebar laguage name and authentication in PagesController

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,6 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: %i[landing dashboard],
-  layout 'landing', only: %i[landing dashboard ]
+  skip_before_action :authenticate_user!, only: %i[landing dashboard]
+  layout 'landing', only: %i[landing]
   
 
   def landing
