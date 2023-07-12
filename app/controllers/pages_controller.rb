82f5bb353d3644337710b3aa455cb2eefe5fc8b7
Lucas Vilela Souza <souza.vilela.lucas@gmail.com>
2023-07-12 16:38:26 +0200

    set   skip_before_action to prompt and recordings

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:landing]
+  skip_before_action :authenticate_user!, only: %i[landing dashboard]
   layout 'landing', only: [:landing]
   
 
--- app/controllers/recordings_controller.rb
+++ app/controllers/recordings_controller.rb
@@ -1,4 +1,6 @@
 class RecordingsController < ApplicationController
+  skip_before_action :authenticate_user!, only: %i[new create]
+
   def new
       @recording = Recording.new
       @recordings = Recording.all
